98c9b69d3208b9c53da49ef8f6780991bb64cf34
siddharth <siddharthmthapa@gmail.com>
2025-05-29 09:30:44 +0530

    home page

--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -1,35 +1,79 @@
-import React, { useEffect } from "react";
 import MainLayout from "../layouts/MainLayout";
 import "../styles/home.scss";
+import { useEffect, useState } from "react";
+import axios from "axios";
 
 const Home = () => {
 
-    // useEffect(() => {
-    //   const closeNav = () => {
-    //     document.body.classList.toggle("nav-visible");
-    //   };
-  
-    //   // Close menu when switching tabs
-    //   closeNav();
-    // }, [location.pathname]); 
-  return (
+  const api = import.meta.env.VITE_API_URL;
+  const [videoSrc, setVideoSrc] = useState('');
+  const [home, setHome] = useState({});
+  useEffect(()=>{
+    async function getData(){
+      try{
+        const res = await axios.get(api + '/homecms')        
+        setHome(res.data.data)
+      }catch(err){
+        console.log(err)
+      }
+    }
+    getData();
+  }, [])
+
+  useEffect(()=>{
+    const mediaQuery = window.matchMedia("(max-width: 768px)");
+    const setVideo = () => {
+      setVideoSrc(
+        mediaQuery.matches
+          ? home.mobile_full_video
+          : home.desktop_full_video
+      );
+    };
+
+    setVideo()
+
+    mediaQuery.addEventListener("change", setVideo);    
+    return () => mediaQuery.removeEventListener("change", setVideo);
+
+  }, [home])
+
+  const [loading, setLoading] = useState(true);
+  const handleLoadingScreen = (action) => {
+    setTimeout(() => {
+      setLoading(action)
+    }, 1000);
+  }
+
+  return (    
     <MainLayout>
-<div className="home">
-  <div className="video-container">
-    <video
-      autoPlay
-      muted
-      loop
-      playsInline
-      className="background-video"
-    >
-      <source src="/assets/home/homebannervideo.mp4" type="video/mp4" />
-      Your browser does not support the video tag.
-    </video>
+
+      {
+        loading && 
+        <div className="loadingScreen">
+          <img src="/assets/gif/BEI_logo.gif" alt="" />
+        </div>
+      }
+
+      {
+        videoSrc && 
+        <div className="home">
+          <div className="video-container">
+            <video
+              autoPlay
+              muted
+              loop
+              playsInline
+              className="background-video"
+              onLoadedData={()=>handleLoadingScreen(false)}
+            >
+            <source src={videoSrc} type="video/mp4" />
+              Your browser does not support the video tag.
+            </video>
 
 
-  </div>
-</div>
+          </div>
+        </div>
+      }
     </MainLayout>
   );
 };
