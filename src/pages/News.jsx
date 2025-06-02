9e8694c8dd12893d3bc09fae753918ee3d936433
siddharth <siddharthmthapa@gmail.com>
2025-06-02 12:15:08 +0530

    news

--- src/pages/News.jsx
+++ src/pages/News.jsx
@@ -3,6 +3,7 @@ import Navbar from "../components/Navbar";
 import Footer from "../components/Footer";
 import "../styles/news.scss";
 import { useEffect, useState } from "react";
+import Slider from "react-slick";
 
 const News = () => {
 
@@ -12,8 +13,18 @@ const News = () => {
       setLoading(action)
     }, 2500);
   }
-
   handleLoadingScreen(false)
+  
+  const sliderSettings = {
+    dots: false,
+    infinite: true,
+    speed: 800,
+    slidesToShow: 2,
+    slidesToScroll: 1,
+    // autoplay: true,
+    // autoplaySpeed: 3000,
+  };
+
 
   return (
     <>
@@ -25,6 +36,50 @@ const News = () => {
       }
       <Navbar />
       <div className="news">
+        <img src="assets/news/news.png" alt="img" className="img1" />
+        <img src="assets/news/tag.jpg" alt="img" className="img2" />
+        <div className="news-slider">
+          <Slider {...sliderSettings} className="brand-slider">
+            <div className="slide">
+              <div className="slide-image">          
+                <h2 className="text">Title</h2>           
+                <img 
+                  src='/assets/8PM_banner.jpg'
+                  alt="" 
+                  loading="lazy"
+                  decoding="async"
+                  fetchPriority="high"                  
+                />
+              </div>
+            </div>
+            
+            <div className="slide">
+              <div className="slide-image">          
+                <h2 className="text">Title</h2>     
+                <img 
+                  src='/assets/8PM_banner.jpg'
+                  alt="" 
+                  loading="lazy"
+                  decoding="async"
+                  fetchPriority="high"                  
+                />
+              </div>
+            </div>
+            <div className="slide">
+              <div className="slide-image">          
+                <h2 className="text">Title</h2>           
+                <img 
+                  src='/assets/8PM_banner.jpg'
+                  alt="" 
+                  loading="lazy"
+                  decoding="async"
+                  fetchPriority="high"                  
+                />
+              </div>
+            </div>
+
+          </Slider>
+        </div>
 
       </div>
       <Footer />
