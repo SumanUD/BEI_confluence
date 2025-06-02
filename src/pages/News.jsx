00957c5cde693b0ce1df3360c50360947376e31d
siddharth <siddharthmthapa@gmail.com>
2025-06-02 12:57:56 +0530

    news api integration

--- src/pages/News.jsx
+++ src/pages/News.jsx
@@ -4,8 +4,27 @@ import Footer from "../components/Footer";
 import "../styles/news.scss";
 import { useEffect, useState } from "react";
 import Slider from "react-slick";
+import 'slick-carousel/slick/slick.css';
+import 'slick-carousel/slick/slick-theme.css';
+import axios from "axios";
 
 const News = () => {
+
+  const api = import.meta.env.VITE_API_URL;
+  const [news, setNews] =useState([])
+
+  useEffect(()=>{
+    async function getNew(){
+      try{
+        const res = await axios.get(api + '/news')        
+        setNews(res.data.data)
+        console.log(res.data.data)
+      }catch(err){
+        console.log(err)
+      }
+    }
+    getNew()
+  },[])
 
   const [loading, setLoading] = useState(true);
   const handleLoadingScreen = (action) => {
@@ -21,6 +40,20 @@ const News = () => {
     speed: 800,
     slidesToShow: 2,
     slidesToScroll: 1,
+      responsive: [
+    {
+      breakpoint: 1024, // below 1024px
+      settings: {
+        slidesToShow: 2,
+      },
+    },
+    {
+      breakpoint: 968, // below 768px
+      settings: {
+        slidesToShow: 1,
+      },
+    },
+  ],
     // autoplay: true,
     // autoplaySpeed: 3000,
   };
@@ -40,20 +73,24 @@ const News = () => {
         <img src="assets/news/tag.jpg" alt="img" className="img2" />
         <div className="news-slider">
           <Slider {...sliderSettings} className="brand-slider">
-            <div className="slide">
-              <div className="slide-image">          
-                <h2 className="text">Title</h2>           
-                <img 
-                  src='/assets/8PM_banner.jpg'
-                  alt="" 
-                  loading="lazy"
-                  decoding="async"
-                  fetchPriority="high"                  
-                />
-              </div>
-            </div>
+            {
+              news?.map((item, index)=>(
+                <div className="slide" key={index}>                 
+                  <div className="slide-image">          
+                      <h2 className="text">{item.news_title}</h2>           
+                      <img 
+                        src={item.thumbnail_picture}
+                        alt="" 
+                        loading="lazy"
+                        decoding="async"
+                        fetchPriority="high"                  
+                      />
+                    </div>
+                </div>
+              ))
+            }
             
-            <div className="slide">
+            {/* <div className="slide">
               <div className="slide-image">          
                 <h2 className="text">Title</h2>     
                 <img 
@@ -64,8 +101,8 @@ const News = () => {
                   fetchPriority="high"                  
                 />
               </div>
-            </div>
-            <div className="slide">
+            </div> */}
+            {/* <div className="slide">
               <div className="slide-image">          
                 <h2 className="text">Title</h2>           
                 <img 
@@ -76,7 +113,7 @@ const News = () => {
                   fetchPriority="high"                  
                 />
               </div>
-            </div>
+            </div> */}
 
           </Slider>
         </div>
