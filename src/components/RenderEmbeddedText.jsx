038c05b865761e88d41dea09314e62ead47eedc9
siddharth <siddharthmthapa@gmail.com>
2025-06-04 19:31:49 +0530

    embaded text correction

--- src/components/RenderEmbeddedText.jsx
+++ src/components/RenderEmbeddedText.jsx
@@ -1,21 +1,7 @@
 import DOMPurify from 'dompurify';
 
 export const RenderEmbeddedText = ({ embeddedText }) => {
-  if (!embeddedText || typeof embeddedText !== 'string') {
-    return null; // or <p>No content available</p>
-  }
-
-  // Decode Unicode escapes like \u003C
-  const unicodeDecoded = embeddedText.replace(/\\u([\dA-F]{4})/gi, (_, g) =>
-    String.fromCharCode(parseInt(g, 16))
-  );
-
-  // Decode HTML entities (like &amp;)
-  const parser = new DOMParser();
-  const htmlDecoded = parser.parseFromString(unicodeDecoded, 'text/html').documentElement.textContent;
-
-  // Sanitize
-  const sanitizedHTML = DOMPurify.sanitize(htmlDecoded);
+  const sanitizedHTML = DOMPurify.sanitize(embeddedText);
 
   return <div dangerouslySetInnerHTML={{ __html: sanitizedHTML }} />;
 };
