1539cc22e74a6161eb0ae05274f1af0bd8b47909
siddharth <siddharthmthapa@gmail.com>
2025-06-04 18:25:37 +0530

    brand page

--- src/components/Gallery.jsx
+++ src/components/Gallery.jsx
@@ -1,4 +1,4 @@
-import React, { useState } from "react";
+import React, { useEffect, useState } from "react";
 import Lightbox from "yet-another-react-lightbox";
 import "yet-another-react-lightbox/styles.css";
 import "bootstrap/dist/css/bootstrap.min.css";
@@ -7,16 +7,26 @@ import "../styles/ImageGallery.scss";
 const ImageGallery = ({ images = [] }) => {
   const [open, setOpen] = useState(false);
   const [currentIndex, setCurrentIndex] = useState(0);
+  const [columnsImage, setColumnsImage] = useState([[], [], [], []]);
 
   const openLightbox = (index) => {
     setCurrentIndex(index);
     setOpen(true);
   };
 
+  useEffect(() => {
+    const columns = Array.from({ length: 4 }, () => []);
+    images.forEach((img, i) => {
+      columns[i % 4].push(img);
+    });
+    setColumnsImage(columns);
+    console.log(columns); // ✅ Now you'll see your 4-column arrays
+  }, [images]);
+
   return (
     <div className="imageGallery">
       <div className="container">
-        <div className="row">
+        {/* <div className="row">
           {images.map((image, index) => (
             <div key={index} className="col-md-3 mb-3">
               <img
@@ -28,7 +38,26 @@ const ImageGallery = ({ images = [] }) => {
               />
             </div>
           ))}
+        </div> */}
+
+        <div className="row">
+          {columnsImage.map((columnImg, colIndex) => (
+            <div key={colIndex} className="col-md-3">
+              {columnImg.map((image, index) => (
+                <div key={index} className="mb-3">
+                  <img
+                    src={image}
+                    className="img-fluid rounded"
+                    alt={image.alt || `Gallery Image ${index + 1}`}
+                    onClick={() => openLightbox(colIndex * 10 + index)} // Adjust index mapping if needed
+                    style={{ cursor: "pointer" }}
+                  />
+                </div>
+              ))}
+            </div>
+          ))}
         </div>
+
 
         {/* Lightbox */}
         <Lightbox
